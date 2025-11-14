f66c7dae83d4f5c565feb6a1afe4af0c0fbfc86e
Jainras-Peter <jainras2004@gmail.com>
2025-11-14 12:20:40 +0530

    More to go

--- frontend/src/app/users/users-module.ts
+++ frontend/src/app/users/users-module.ts
@@ -4,20 +4,22 @@ import { UsersList } from './users-list/users-list';
 import { UserModal } from './user-modal/user-modal';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MessageModule } from '../message/message-module';
+import { UserFilter } from './user-filter/user-filter';
+
 
 
 
 @NgModule({
   declarations: [
     UsersList,
-    UserModal
+    UserModal,
+    UserFilter
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-
-    MessageModule
+    MessageModule,
   ],
   exports: [
     UsersList 
